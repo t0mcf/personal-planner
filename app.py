d69dd0034dc2510ab43408ca3dfa311dce84142a
Tom <fischer.tom63@gmail.com>
2025-12-20 19:01:47 +0900

    fixed pointer log message showing up while running app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
+import os 
 
 from ui.main_window import MainWindow
 from db import connect_db, init_db, insert_test_transaction, get_all_transactions
 
 
 def main():
+    os.environ['QT_LOGGING_RULES'] = 'qt.pointer.dispath=false' #to get rid of annoying log message
     db_connection = connect_db()
     init_db(db_connection)    
     db_connection.close()
